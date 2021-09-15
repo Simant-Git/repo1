5851aaa3da21ecbdd98c0b35c7ddb322b1e15245
Simant-Git <simant.setu@hotmail.com>
2021-09-15 14:43:49 +0530

    modified ticket class Simant

--- LtiAirlines/src/main/java/com/lti/model/Ticket.java
+++ LtiAirlines/src/main/java/com/lti/model/Ticket.java
@@ -1,8 +1,30 @@
 package com.lti.model;
 
+import java.time.LocalDate;
+
 public class Ticket {
 	int ticketNo;
 	int seatNo;
+	LocalDate travelDate;
+	public int getTicketNo() {
+		return ticketNo;
+	}
+	public void setTicketNo(int ticketNo) {
+		this.ticketNo = ticketNo;
+	}
+	public int getSeatNo() {
+		return seatNo;
+	}
+	public void setSeatNo(int seatNo) {
+		this.seatNo = seatNo;
+	}
+	public LocalDate getTravelDate() {
+		return travelDate;
+	}
+	public void setTravelDate(LocalDate travelDate) {
+		this.travelDate = travelDate;
+	}
+	
 	
 
 }
