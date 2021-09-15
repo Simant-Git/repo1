09501ef52e471a32f16c52fa1aae2635b0b993cf
nikhilamateti2708 <nanikki2000@gmail.com>
2021-09-15 14:45:59 +0530

    modified ticket class

--- LtiAirlines/src/main/java/com/lti/model/Ticket.java
+++ LtiAirlines/src/main/java/com/lti/model/Ticket.java
@@ -3,6 +3,18 @@ package com.lti.model;
 public class Ticket {
 	int ticketNo;
 	int seatNo;
+	public int getTicketNo() {
+		return ticketNo;
+	}
+	public void setTicketNo(int ticketNo) {
+		this.ticketNo = ticketNo;
+	}
+	public int getSeatNo() {
+		return seatNo;
+	}
+	public void setSeatNo(int seatNo) {
+		this.seatNo = seatNo;
+	}
 	
 
 }
